5f50081b926b38e9422dbfdccaa37ae0ec9ebc69
Axelco201 <axel.sangoku0@gmail.com>
2026-05-29 19:38:55 +0000

    Add files via upload

---
+++ main.d
@@ -0,0 +1,36 @@
+import std.stdio;
+import std.random;
+import std.math;
+
+
+auto inCircle(double x, double y){
+	if( sqrt(x*x + y*y) <= 1){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void main(){
+	auto rnd = Random(unpredictableSeed);
+	int nbPoint = 0;
+	int nbPointInCircle = 0;	
+	const auto limite = 10000000 - 1;
+	int i = 0;
+	while(i <= limite){
+		double x = uniform(-1.0, 1.0, rnd);
+		//writeln(x);
+		double y = uniform(-1.0, 1.0, rnd);
+		//writeln(y);
+
+		nbPoint++;
+
+		if (inCircle(x,y)){
+			nbPointInCircle++;
+		}
+		i++;
+	}
+
+	writeln("nbPointInCircle : ", nbPointInCircle, "\nnbPoint : ", nbPoint, "\nPI = ", (nbPointInCircle/float(nbPoint))*4.0 );
+}
